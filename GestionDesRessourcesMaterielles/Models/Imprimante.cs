2eaf511aa60066fa3354e4373903390f8a162a4e
1renji <naoufalhary@gmail.com>
2024-04-28 22:19:16 +0100

    Imprimante.cs new class model

---
+++ GestionDesRessourcesMaterielles/Models/Imprimante.cs
@@ -0,0 +1,9 @@
+﻿namespace GestionDesRessourcesMaterielles.Models
+{
+    public class Imprimante :Ressource
+    {
+        private int Vitesseimpression { get; set; }
+        private string Resolution { get; set; }
+        private string Marque { get; set; }
+    }
+}
