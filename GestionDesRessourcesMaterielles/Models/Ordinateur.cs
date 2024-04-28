f939144f62b94acb6429b02b6336fa2cd354ef65
1renji <naoufalhary@gmail.com>
2024-04-28 22:19:31 +0100

    Ordinateur.cs new class model

---
+++ GestionDesRessourcesMaterielles/Models/Ordinateur.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionDesRessourcesMaterielles.Models
+{
+    public class Ordinateur : Ressource
+    {
+        private string Marque { get; set; }
+        private string Cpu { get; set; }    
+        private string Ram { get; set; }
+        private string DisqueDur { get; set; }
+        private string Ecran { get; set; }
+
+    }
+}
