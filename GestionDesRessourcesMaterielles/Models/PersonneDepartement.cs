c1d25ecb025b269cbae9c73a21725cb4df475779
1renji <naoufalhary@gmail.com>
2024-04-28 22:43:18 +0100

    PersonneDepartement.cs new class model

---
+++ GestionDesRessourcesMaterielles/Models/PersonneDepartement.cs
@@ -0,0 +1,17 @@
+﻿namespace GestionDesRessourcesMaterielles.Models
+{
+    public class PersonneDepartement
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private Role Role { get; set; }
+        
+
+    }
+
+    public enum Role
+    {
+        enseignant,
+        administratif
+    }
+}
