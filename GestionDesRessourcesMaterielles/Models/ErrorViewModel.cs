ab16871e23ce3bb24dcd229898d773037bc31b3a
1renji <naoufalhary@gmail.com>
2024-04-28 17:30:22 +0100

    Delete folder

--- GestionDesRessourcesMaterielles/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GestionDesRessourcesMaterielles.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
