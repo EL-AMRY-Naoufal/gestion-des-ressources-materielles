2c7d3473cef3c0be482dcdc794306560779f00f7
1renji <naoufalhary@gmail.com>
2024-04-28 22:18:24 +0100

    Ressource.cs new class model

---
+++ GestionDesRessourcesMaterielles/Models/Ressource.cs
@@ -0,0 +1,9 @@
+﻿namespace GestionDesRessourcesMaterielles.Models
+{
+    public class Ressource
+    {
+        private int NumeroInventaire { get; set; }
+        private DateTime DateLivraison { get; set; }
+        private int FournisseurId { get; set; }
+    }
+}
