aa23c901c6e44cf21ddc9aa1c5e2317df5d88eb5
1renji <naoufalhary@gmail.com>
2024-04-28 23:03:18 +0100

    PersonneDepartement.cs update

--- GestionDesRessourcesMaterielles/Models/PersonneDepartement.cs
+++ GestionDesRessourcesMaterielles/Models/PersonneDepartement.cs
@@ -5,8 +5,7 @@
         private int Id { get; set; }
         private string Name { get; set; }
         private Role Role { get; set; }
-        
-
+        private string? Laboratoire { get; set; } // class ??
     }
 
     public enum Role
