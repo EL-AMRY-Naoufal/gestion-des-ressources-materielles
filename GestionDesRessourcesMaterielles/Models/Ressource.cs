d816e81ef9f54beaf9942dcd32b56dab19814317
1renji <naoufalhary@gmail.com>
2024-04-28 23:03:48 +0100

    Ressource.cs update

--- GestionDesRessourcesMaterielles/Models/Ressource.cs
+++ GestionDesRessourcesMaterielles/Models/Ressource.cs
@@ -5,5 +5,7 @@
         private int NumeroInventaire { get; set; }
         private DateTime DateLivraison { get; set; }
         private int FournisseurId { get; set; }
+        private int? PersonneDepartement { get; set; }
+        private int? Departement { get; set; }
     }
 }
