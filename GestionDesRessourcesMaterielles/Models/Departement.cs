3d42417f76a8f368485886a8d29ac60b00e10415
1renji <naoufalhary@gmail.com>
2024-04-29 21:07:15 +0100

    Departement.cs Update

--- GestionDesRessourcesMaterielles/Models/Departement.cs
+++ GestionDesRessourcesMaterielles/Models/Departement.cs
@@ -2,5 +2,9 @@
 {
     public class Departement
     {
+        private int DepartmentId { get; set; }
+        private float Budget { get; set; }
+        private int ChefDepartementId { get; set; }
+
     }
 }
