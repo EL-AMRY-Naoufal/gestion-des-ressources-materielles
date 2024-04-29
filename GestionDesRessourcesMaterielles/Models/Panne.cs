305a55b5da30d6fb3b04ffc4e386b2247d7f2ea1
1renji <naoufalhary@gmail.com>
2024-04-29 19:39:49 +0100

    Panne.cs new class model

---
+++ GestionDesRessourcesMaterielles/Models/Panne.cs
@@ -0,0 +1,25 @@
+﻿namespace GestionDesRessourcesMaterielles.Models
+{
+    public class Panne
+    {
+        private int PanneId {  get; set; }
+        private string Explication { get; set; }
+        private DateTime DateApparition { get; set; }
+        private Frequence frequence { get; set; }
+        private ClassificationPanne ClassificationPanne { get; set; }
+    }
+
+    public enum Frequence
+    {
+        rare,
+        frequente,
+        permanente
+    }
+
+    public enum ClassificationPanne
+    {
+        DefautSysteme,
+        LogicielUtilitaire,
+        Materiel
+    }
+}
