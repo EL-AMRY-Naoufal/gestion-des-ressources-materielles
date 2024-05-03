a58cabf822253bcb85035fc847e07c42ff46365a
1renji <naoufalhary@gmail.com>
2024-05-03 23:11:38 +0100

    User.cs new class model

---
+++ GestionDesRessourcesMaterielles/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace GestionDesRessourcesMaterielles.Models
+{
+    public class User
+    {
+        private string Email { get; set; }  
+        private string Password { get; set; }
+    }
+}
