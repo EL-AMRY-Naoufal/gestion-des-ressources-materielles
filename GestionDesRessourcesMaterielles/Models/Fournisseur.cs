7f68de0889926610d9fea24a17cdf6f3183a3896
1renji <naoufalhary@gmail.com>
2024-05-03 23:03:06 +0100

    Fournisseur.cs new class model

---
+++ GestionDesRessourcesMaterielles/Models/Fournisseur.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionDesRessourcesMaterielles.Models
+{
+    public class Fournisseur
+    {
+        private int FournisseurId { get; set; }
+        private string Name { get; set; }
+
+        private string NomSociete { get; set; }
+        private string Lieu { get; set; }
+        private string Gerant { get; set; }
+    }
+}
