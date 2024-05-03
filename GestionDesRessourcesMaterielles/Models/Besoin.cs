0198ff9c1c6becc66f3c6c07d60cd4fe928af005
1renji <naoufalhary@gmail.com>
2024-05-03 23:01:42 +0100

    Besoin.cs new class model

---
+++ GestionDesRessourcesMaterielles/Models/Besoin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionDesRessourcesMaterielles.Models
+{
+    public class Besoin
+    {
+        private int BesoinId { get; set; }
+        private int Quantite { get; set; }
+        [ForeignKey("RessourceId")]
+        private Ressource Ressource { get; set; }
+    }
+}
