163807de16e7b9f635ee7ffe44344030f04a6868
1renji <naoufalhary@gmail.com>
2024-05-06 23:06:14 +0100

    Program.cs Update

--- GestionDesRessourcesMaterielles/Program.cs
+++ GestionDesRessourcesMaterielles/Program.cs
@@ -30,6 +30,23 @@ namespace GestionDesRessourcesMaterielles
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
 
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryverysecret......")),
+                    ValidateAudience = false,
+                    ValidateIssuer = false
+                };
+            });
+
             var app = builder.Build();
 
             app.UseSwagger();
@@ -50,22 +67,7 @@ namespace GestionDesRessourcesMaterielles
                 app.UseHsts();
             }
 
-            builder.Services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryverysecret......")),
-                    ValidateAudience = false,
-                    ValidateIssuer = false
-                };
-            });
+            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
