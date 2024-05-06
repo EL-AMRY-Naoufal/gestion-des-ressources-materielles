39a38861d5d0519148f15e8defd1af7c432f31da
1renji <naoufalhary@gmail.com>
2024-05-06 03:01:11 +0100

    Program.cs Update

--- GestionDesRessourcesMaterielles/Program.cs
+++ GestionDesRessourcesMaterielles/Program.cs
@@ -2,6 +2,9 @@ using GestionDesRessourcesMaterielles.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace GestionDesRessourcesMaterielles
 {
@@ -46,6 +49,23 @@ namespace GestionDesRessourcesMaterielles
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryverysecret......")),
+                    ValidateAudience = false,
+                    ValidateIssuer = false
+                };
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
