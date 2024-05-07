690ace1927b0203b268cbb155a5ffca228f5f527
1renji <naoufalhary@gmail.com>
2024-05-07 03:04:13 +0100

    app.routes.ts Updatre

--- GRM-Client/GestionDesRessourcesMaterielles-Client/src/app/app.routes.ts
+++ GRM-Client/GestionDesRessourcesMaterielles-Client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
