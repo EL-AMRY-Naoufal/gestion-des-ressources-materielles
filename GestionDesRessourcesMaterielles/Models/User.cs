cea76152de830eb7fd4dc66f5adf816622eeb104
1renji <naoufalhary@gmail.com>
2024-05-07 01:28:02 +0100

    User.cs Update

--- GestionDesRessourcesMaterielles/Models/User.cs
+++ GestionDesRessourcesMaterielles/Models/User.cs
@@ -9,6 +9,5 @@ namespace GestionDesRessourcesMaterielles.Models
         public int UserId { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Token { get; set; }
     }
 }
