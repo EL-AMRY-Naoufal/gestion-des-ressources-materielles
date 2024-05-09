a3efde81033fcbb30e250cd33693edabd111d5b2
1renji <naoufalhary@gmail.com>
2024-05-09 23:57:18 +0100

    app full repo

--- GRM-Client/GestionDesRessourcesMaterielless/src/app/components/main-layout/main-layout.component.ts
+++ GRM-Client/GestionDesRessourcesMaterielless/src/app/components/main-layout/main-layout.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements AfterViewInit, OnInit {
+  @ViewChild(SidebarComponent, { static: true }) sidebarComponent!: SidebarComponent;
 
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.sidebarComponent.sidebarStatusChanged.subscribe(({ isOpen, width }) => {
+      this.updateNavBarWidth(isOpen, width);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.sidebarComponent.sidebarStatusChanged.subscribe(({ isOpen, width }) => {
+      this.updateNavBarWidth(isOpen, width);
+    });
+  }
+
+  private updateNavBarWidth(isSidebarOpen: boolean, navBarWidth: number): void {
+    const navBarElement = this.elementRef.nativeElement.querySelector('#nav-bar');
+    if (navBarElement) {
+      navBarElement.style.width = `${navBarWidth}px`;
+    }
+  }
 }
--- GRM-Client/GestionDesRessourcesMaterielless/src/app/components/sidebar/sidebar.component.ts
+++ GRM-Client/GestionDesRessourcesMaterielless/src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  @Output() sidebarStatusChanged = new EventEmitter<{ isOpen: boolean, width: number }>();
+  isSidebarOpen = false; // Assuming sidebar is initially open
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.sidebarStatusChanged.emit({ isOpen: this.isSidebarOpen, width: this.getBodyWidth() });
+  }
+
+  toggleSidebar() {
+    // Toggle the sidebar status
+    this.isSidebarOpen = !this.isSidebarOpen;
+    
+    // Emit the updated sidebar status and width after a short delay
+    setTimeout(() => {
+      this.emitSidebarStatusAndWidth();
+    }, 200); // Adjust the delay time as needed based on your sidebar animation duration
+  }
+
+  private emitSidebarStatusAndWidth(): void {
+    const width = this.getBodyWidth();
+    this.sidebarStatusChanged.emit({ isOpen: this.isSidebarOpen, width });
+  }
+
+  private getBodyWidth(): number {
+    const bodyElement = this.elementRef.nativeElement.querySelector('#nav-bar');
+    console.log(bodyElement.offsetWidth);
+    return bodyElement.offsetWidth + 30;
+  }
 }
