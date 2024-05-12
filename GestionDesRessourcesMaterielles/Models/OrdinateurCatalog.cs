f903436ea12b6b9a33286dfc7dec7c0e6542949c
1renji <naoufalhary@gmail.com>
2024-05-12 16:47:15 +0100

    OrdinateurCatalog.cs new model class

---
+++ GestionDesRessourcesMaterielles/Models/OrdinateurCatalog.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionDesRessourcesMaterielles.Models
+{
+    public class OrdinateurCatalog
+    {
+        [Key]
+        public string ImprimanteID { get; set; }
+        public string Marque { get; set; }
+        public string Cpu { get; set; }
+        public string Ram { get; set; }
+        public string DisqueDur { get; set; }
+        public string Ecran { get; set; }
+    }
+}
