468f372d7117eb380a9d33839dce788a126506bc
1renji <naoufalhary@gmail.com>
2024-05-12 16:46:45 +0100

    ImprimanteCatalog.cs new model class

---
+++ GestionDesRessourcesMaterielles/Models/ImprimanteCatalog.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionDesRessourcesMaterielles.Models
+{
+    public class ImprimanteCatalog
+    {
+        [Key]
+        public string ImprimanteID { get; set; }
+        public int Vitesseimpression { get; set; }
+        public string Resolution { get; set; }
+        public string Marque { get; set; }
+    }
+}
