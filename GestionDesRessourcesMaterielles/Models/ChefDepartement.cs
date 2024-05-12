c2fdc1d257f5307f62f44d0ffcae770da020aaad
1renji <naoufalhary@gmail.com>
2024-05-12 23:18:19 +0100

    ChefDepartement.cs update

--- GestionDesRessourcesMaterielles/Models/ChefDepartement.cs
+++ GestionDesRessourcesMaterielles/Models/ChefDepartement.cs
@@ -1,6 +1,11 @@
-﻿namespace GestionDesRessourcesMaterielles.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionDesRessourcesMaterielles.Models
 {
     public class ChefDepartement : User
     {
+        public string Name { get; set; }
+        [ForeignKey("DepartementID")]
+        public Departement Departement { get; set; }
     }
 }
