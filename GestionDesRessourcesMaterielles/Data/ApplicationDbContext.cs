c5d0ec392782f160c5aff1d56ef9143a4a554d30
1renji <naoufalhary@gmail.com>
2024-05-15 16:09:53 +0100

    ApplicationDbContext.cs

--- GestionDesRessourcesMaterielles/Data/ApplicationDbContext.cs
+++ GestionDesRessourcesMaterielles/Data/ApplicationDbContext.cs
@@ -21,7 +21,9 @@ namespace GestionDesRessourcesMaterielles.Data
         public DbSet<Panne> Pannes { get; set; }
         public DbSet<PersonneDepartement> PersonneDepartements { get; set; }
         public DbSet<ResponsableRessources> ResponsableRessources { get; set; }
-        public DbSet<Ressource> Ressources { get; set; }
         public DbSet<ServiceMaintenance> ServiceMaintenances { get; set; }
+        public DbSet<ImprimanteCatalog> ImprimanteCatalog { get; set; }
+        public DbSet<OrdinateurCatalog> OrdinateurCatalog { get; set;  }
+        public DbSet<OffreFournisseur> offreFournisseurs { get; set; }
     }
 }
