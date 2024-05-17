0467e2615dc055d7e41a38b9f43c0673781a01ff
1renji <naoufalhary@gmail.com>
2024-05-17 17:48:57 +0100

    app-routing.module.ts Update

--- GRM-Client/GestionDesRessourcesMaterielless/src/app/app-routing.module.ts
+++ GRM-Client/GestionDesRessourcesMaterielless/src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { LoginComponent } from './components/login/login.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { AuthGuard } from './guards/auth.guard';
 import { ListRessourcesComponent } from './components/list-ressources/list-ressources.component';
+import { ListeBesoinComponent } from './components/liste-besoin/liste-besoin.component';
+import { SendBesoinsComponent } from './components/send-besoins/send-besoins.component';
+import { AppelOffresComponent } from './components/appel-offres/appel-offres.component';
+import { OffreFournisseurComponent } from './components/offre-fournisseur/offre-fournisseur.component';
+import { AcceptedFournisseurOffreComponent } from './components/accepted-fournisseur-offre/accepted-fournisseur-offre.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -14,6 +19,11 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'listeRessources', component:  ListRessourcesComponent}, 
+      {path : 'listBesoins', component : ListeBesoinComponent},
+      {path : 'sendBesoins', component : SendBesoinsComponent},
+      {path : 'appelOffres', component : AppelOffresComponent},
+      {path : 'offreFournisseur', component : OffreFournisseurComponent},
+      {path : 'acceptedOffreFournisseur', component : AcceptedFournisseurOffreComponent}
     ]
   },
 ];
